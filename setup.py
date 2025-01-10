d898d6ca352f44ecae0e65ef51b8fa6d8c118981
Senthur pandian <skpandian8@gmail.com>
2025-01-10 05:48:39 +0530

    final commit

--- setup.py
+++ setup.py
@@ -1,35 +1,52 @@
-from cx_Freeze import setup,Executable,sys
-includefiles=['icon.ico']
-excludes=[]
-packages=[]
-base=None
-if sys.platform=="win32":
-    base="Win32GUI"
+from cx_Freeze import setup, Executable
+import sys
 
-shortcut_table=[
-    ("DesktopShortcut",
-     "DesktopFolder",
-     "Billing System",
-     "TARGETDIR",
-     "[TARGETDIR]\main.exe",
-     None,
-     None,
-     None,
-     None,
-     None,
-     None,
-     "TARGETDIR",
-     )
+# Include files to be bundled with the executable
+includefiles = ['icon.ico']
+
+# Dependencies
+excludes = []  # List any modules to exclude from the build (if necessary)
+packages = []  # List any additional packages to include (if necessary)
+
+# Define base for Windows GUI application
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# Define the MSI shortcut table
+shortcut_table = [
+    (
+        "DesktopShortcut",            # Shortcut name
+        "DesktopFolder",              # Directory where shortcut will be created
+        "Billing System",             # Display name of the shortcut
+        "TARGETDIR",                  # Target directory
+        "[TARGETDIR]main.exe",        # Path to the executable
+        None,                         # Arguments for the shortcut
+        None,                         # Description
+        None,                         # Hotkey
+        None,                         # Icon (default)
+        None,                         # Icon index
+        None,                         # ShowCmd (normal window)
+        "TARGETDIR",                  # Working directory
+    )
 ]
-msi_data={"Shortcut":shortcut_table}
 
-bdist_msi_options={'data':msi_data}
+# MSI data with shortcuts
+msi_data = {"Shortcut": shortcut_table}
+
+# MSI options
+bdist_msi_options = {'data': msi_data}
+
+# Setup configuration
 setup(
-    version="0.1",
-    description="Billing System",
-    author="faizan Khan",
     name="Billing System",
-    options={'build_exe':{'include_files':includefiles},'bdist_msi':bdist_msi_options,},
+    version="0.1",
+    description="A simple Billing System application",
+    author="Faizan Khan",
+    options={
+        'build_exe': {'include_files': includefiles},
+        'bdist_msi': bdist_msi_options,
+    },
     executables=[
         Executable(
             script="main.py",
